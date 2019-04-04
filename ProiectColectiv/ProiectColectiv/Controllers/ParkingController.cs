d7909f69e923545a8c4f542c1079eca0ac3fc233
CristinaGh <38916791+cristinagh@users.noreply.github.com>
2019-04-04 14:20:22 +0300

    Hello world

---
+++ ProiectColectiv/ProiectColectiv/Controllers/ParkingController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ProiectColectiv.Models;
+
+namespace ProiectColectiv.Controllers
+{
+    public class ParkingController : Controller
+    {
+        private ProiectColectivEntities db = new ProiectColectivEntities();
+        //Index -TODO
+
+        //Create-TODO
+
+        //Edit
+        // GET: Parcare/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Parcare parcare = await db.Parcare.FindAsync(id);
+                if (parcare == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(parcare);
+            }
+            catch (Exception e)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest); // to do: redirect to error page
+            }
+        }
+        // POST: Parcare/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Nume, Locatie")] Parcare parcare)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(parcare).State = EntityState.Modified;
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                return View(parcare);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
